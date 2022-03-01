5d972ecd730e0be5ef22e93042a39137c2d39b75
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-01 07:36:20 +0700

    adding session support with express-session and connect-mongodb-session package

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+// using express-session
+const session = require("express-session");
+// using connect-mongodb-session for storing the session
+const mongodbStore = require("connect-mongodb-session");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
+
+// create MongoDBStore object with executing mongodbStore as a function and passing the session package as the parameter
+// with that MongoDBStore is actually a Class, a constructor function we can execute to create a new object based on a certain blue prints. And for executing that see at the line 18
+const MongoDBStore = mongodbStore(session);
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const sessionStore = new MongoDBStore({
+  // uri basically same as URL
+  uri: "mongodb://localhost:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "super-secret",
+    // only update the session if the data really change, so we set it to false
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
